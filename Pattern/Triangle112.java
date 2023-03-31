d7f51560845f1fcb408cd5c011dac4cbee919b6b
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-03-31 03:16:50 -0700

    Add files via upload

---
+++ Pattern/Triangle112.java
@@ -0,0 +1,20 @@
+package patternpro;
+
+public class Triangle112 {
+
+	public static void main(String[] args) {
+	
+		for(int i=1;i<=5;i++)
+		{
+			int count=1;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(count++);
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
