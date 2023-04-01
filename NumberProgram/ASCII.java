556bdedbd4e7853ad69ca9043c09c7de621b3da4
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-03-31 22:55:50 -0700

    Add files via upload

---
+++ NumberProgram/ASCII.java
@@ -0,0 +1,16 @@
+package exp;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+	
+		char ch='a';
+		
+		for(int i=ch;i<='z';i++)
+		{
+			System.out.println("the ASCII value of "+ch++ +"="+i);
+		}
+
+	}
+
+}
