a98711c8914c803f7c806192162e443db5a7d44c
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-03-31 21:56:24 -0700

    Add files via upload

---
+++ Conditional Statement/GreatestTwo.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class GreatestTwo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		String res=a>b?"a is greater":"b is Greater";
+		System.out.println(res);
+
+	}
+
+}
