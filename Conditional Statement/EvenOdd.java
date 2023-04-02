7b3b3d747b752e56bf34f0b70a9ac0beb0e6c07d
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-01 22:42:32 -0700

    Add files via upload

---
+++ Conditional Statement/EvenOdd.java
@@ -0,0 +1,25 @@
+package decisionMaking;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number");
+		int num=sc.nextInt();
+		sc.close();
+		
+		if(num%2==0)
+		{
+			System.out.println(num+" is even");
+		}
+		else
+		{
+			System.out.println(num+" is odd");
+		}
+
+	}
+
+}
