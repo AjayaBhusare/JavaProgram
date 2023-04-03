7f96270ee04079605839e665bf9e8134dd6e6642
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-02 20:49:25 -0700

    Add files via upload

---
+++ Conditional Statement/Voting.java
@@ -0,0 +1,23 @@
+package decisionMaking;
+
+import java.util.Scanner;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the age");
+		int age=sc.nextInt();
+
+		if(age>18)
+		{
+			System.out.println("Eligible for Voting");
+		}
+		else
+		{
+			System.out.println("Not Eligible for Voting");
+		}
+		sc.close();
+	}
+
+}
