106c1e44e924afdbf265dab1b222096ce1af9869
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-13 21:05:27 -0700

    Add files via upload

---
+++ Array1D/Char1standLast.java
@@ -0,0 +1,20 @@
+package array;
+
+public class Char1standLast {
+
+	public static void main(String[] args) {
+	
+		char[] arr=new char[5];
+		 arr[0]='r';
+		 arr[1]='a';
+		 arr[2]='j';
+		 arr[3]='m';
+		 arr[4]='p';
+		 
+			 System.out.println("1st array char "+arr[0]);
+			 System.out.println("last array char "+arr[arr.length-1]);
+		 
+
+	}
+
+}
