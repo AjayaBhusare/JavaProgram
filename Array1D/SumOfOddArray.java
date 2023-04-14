09ce8e12891a21afa9732688eb9dc1a1eeeaa4c2
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-14 00:02:42 -0700

    Add files via upload

---
+++ Array1D/SumOfOddArray.java
@@ -0,0 +1,18 @@
+package array;
+
+public class SumOfOddArray {
+
+	public static void main(String[] args) {
+       int a[]= {1,3,2,4,5,67,8,9,6,8};
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(i%2!=0)
+			{
+				sum=sum+a[i];
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
