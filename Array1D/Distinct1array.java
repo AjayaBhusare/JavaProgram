5c302cff0182cf23c94a1eaf4cffcbe4d513a7ad
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-13 21:30:32 -0700

    Add files via upload

---
+++ Array1D/Distinct1array.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Distinct1array {
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 60, 50, 20, 60 };
+		for (int i = 0; i < arr.length; i++) {
+			int count=0;
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+				count++;
+				}
+			}
+			if (count==1) {
+				System.out.println(arr[i]);
+			}	
+		}	
+	}
+}
