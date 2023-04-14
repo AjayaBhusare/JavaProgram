be3faa461056d2cff637a7a3f6e3f774e86c5822
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-13 22:47:40 -0700

    Add files via upload

---
+++ Array1D/DoubleArray.java
@@ -0,0 +1,15 @@
+package array;
+
+public class DoubleArray {
+
+	public static void main(String[] args) {
+		double[] arr= {12.3,45.6,7.8,9.4,43.4,21.3};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+",");
+		}
+
+	}
+
+}
