ad7f5c7d4bc9b740a096ea55129dad758514f2db
Ajaya Bhusare <129386208+AjayaBhusare@users.noreply.github.com>
2023-04-16 00:52:12 -0700

    Add files via upload

---
+++ Array2D/MatrixMultiplication.java
@@ -0,0 +1,40 @@
+package array2d;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+     
+		int[][] a= {{7,2,3},{3,6,5},{3,6,5}};
+		int[][] b= {{1,8,3},{3,4,5},{3,4,9}};
+		int[][] c=new  int[3][3];
+		int col=a.length;
+		int row=b.length;
+		if(row!=col)
+		{
+			System.out.println("Matrix must be Square matrix");
+		}
+		else
+		{
+			System.out.println(" Matrix multiplication");
+			for(int i=0;i<c.length;i++)
+			{
+				for(int j=0;j<c[i].length;j++)
+				{
+					for(int k=0;k<a.length;k++)
+					{
+						c[i][j]+=a[i][k]*b[k][j];
+					}
+				}
+			}
+		}
+		for(int i=0;i<c.length;i++)
+		{
+			for(int j=0;j<c[i].length;j++)
+			{
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
